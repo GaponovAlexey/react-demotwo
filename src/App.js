6c6bb1649b0e9fe8aed44f239eb2bb89e130bf72
Алексей Гапонов <alexei.gaponov@gmail.com>
2021-11-08 15:20:05 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { LocalStorageLear } from './components/LocalStorageLear.jsx';
-import './App.css';
-import { useState } from 'react';
-import { Clicker } from './components/Clicker.jsx';
-import { Ref } from './components/Ref';
-import { Timer } from './components/Timer';
-import { MyTimer } from './components/MyTimer.jsx';
-
 function App() {
-  const [Iscliked, setstate] = useState(false)
 
   return (
-    <div className="App">
-      <h2>React app</h2>
-      {/* <button onClick={ () => setstate(!Iscliked) } >toogle click</button>
-      { Iscliked && < Clicker />}
-      <Ref /> */}
-      <Timer />
-      <MyTimer />
-
+    <div>
+      
     </div>
-  );
+  )
+   
 }
 
 export default App;
